b592a4567940b595f119a667a1545c97a1eda116
丁泽华 <415612687@qq.com>
2020-04-28 08:39:21 +0800

    ship

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var cn = ADODB.open('Provider=Microsoft.ACE.OLEDB.12.0;Data Source='+ "D:\\www\\
 
 var express = require('express');
 var app = express();
+const C = require('./CONSTANT');
 
 app.get('/data',(req,res) => {
   let result = {error : 0,data : ''};
@@ -38,24 +39,33 @@ app.get('/*.*', function (req, res) {
   res.sendFile(convert(req.path));
 });
 
-app.get('/ship_data',function(req,res){
+app.get('/ship_see',async function(req,res){
   if(req.query.ship_no) {
     let ship_no = addSlash(req.query.ship_no);
-    let result = {error : 0,data : null};
-    cn.query(`SELECT * FROM ship_table WHERE ship_no = "${ship_no}"`).then(
-      (data) => {
-        result.data = data;
-      },
-      (error) => {
-        result.error = 1;
-        console.log(error);
-      }
-    ).then(() => {
-      res.send(JSON.stringify(result));
-    });
+    /* 执行中 = 1,完成 = 2, 中断 = 6, 超时 = 7, 删除 = 8 */
+    let sql = `SELECT 
+                ship_table.ship_no AS ship_no,ship_table.number AS \`number\` ,ship_table.sub_number AS sub_number ,ship_table.brand as brand,
+                ship_table.quantity AS quantity,ship_table.ship_time AS ship_time,ship_table.missed AS \`missed\`,
+                ship_table.completed AS completed,ship_state.state_name AS state 
+                FROM ship_table 
+                LEFT JOIN ship_state AS ship_state ON ship_table.state = ship_state.id
+                WHERE ship_no = "${ship_no}" AND state <> 8`;
+    res.send(await queryToJSON(sql));
     return;
   }
-  res.send('error,unFine param ship_data');
+  res.send('Error,unFine param ship_data');
+});
+
+app.get('/ship_modify', async function(req,res) {
+  if(!empty(req.query.ship_no)) {
+    let ship_no = req.query.ship_no;
+    delete req.query.ship_no;
+    if(!empty(req.query)) {
+      res.send(await updateExceute('ship_table',req.query,'ship_no',ship_no));
+    }
+    return;
+  }
+  res.send('Error,params invalid');
 });
 
 var server = app.listen({host:"192.168.0.115",port : 80}, function () {
@@ -66,8 +76,44 @@ var server = app.listen({host:"192.168.0.115",port : 80}, function () {
  
 });
 
+async function query(sql) {
+  let result = {error : 0,data : null};
+  await cn.query(sql).then((data) => { result.data = data;},(error) => { result.error = 1;console.log(error) })
+  return result;
+}
+
+async function queryToJSON(sql) {
+  return JSON.stringify(await query(sql))
+}
+
+async function execute(sql) {
+  let result = {error : 0,data : null};
+  await cn.execute(sql,`SELECT @@Identity AS id`).then((data) => { result.data = data;},(error) => { result.error = 1;console.log(error) })
+  return result;
+}
+
+async function executeToJSon(sql) {
+  return JSON.stringify(await execute(sql))
+}
+
+async function updateExceute(tableName,data,where,whereVal) {
+  let sql = `UPDATE ${tableName} SET `;
+      for(let field in data) {
+        if(data.hasOwnProperty(field))
+          sql += `\`${field}\` = '${addSlash(data[field])}',`;
+      }
+      sql = sql.slice(0,-1);
+      sql += ` WHERE ${where} = '${whereVal}'`;
+  return await executeToJSon(sql);
+}
+
+async function inster(tableName,data) {
+  let sql = `INSERT INTO ${tableName} (\`${Object.keys(data).join('`,`')}\`) VALUES('${Object.values(data).join("','")}')`;
+  return await execute(sql);
+}
 
 function addSlash(str) {
+  if(str.constructor !== String) return str;
   return str.replace(/[']/g,'\\\'').replace(/["]/g,'\\"').replace(/[`]/g,'\\`');
 
 }
@@ -77,6 +123,17 @@ function convert(path) {
   str[0] = str[0].replace('\/',"\\");
   return 'D:\\www' + str.join('.');
 }
+
+function addship(data) {
+  let time = new Date();
+  inster('ship_table',{
+    ship_no : 'ship_' + time.getTime(),
+    ...data,
+    ship_time : time.getFullYear() + '-' + time.getMonth() + '-' + time.getDay()
+  });
+}
+
+
 
 function where(params,order) {
   let where = '';
@@ -94,9 +151,9 @@ function where(params,order) {
           console.log(typeof params[name],typeof range);
           where += name + '>' + (params[name] - range) + ' AND ' + name + '<' + (+params[name] + (+range)) + ' ';
       } else if(name == 'brand') {
-        where += name + ' LIKE "%' + params[name] + '%" ';
+        where += name + ' LIKE "%' + addSlash(params[name]) + '%" ';
       } else {
-        where += name + '=' + params[name] + ' ';
+        where += name + '=' + addSlash(params[name]) + ' ';
       }
     }
   }
@@ -111,6 +168,37 @@ function where(params,order) {
   }
   console.log(where);
   return where;
+}
+
+function getDate(obj) {
+  let time = null
+  if(obj instanceof Date) {
+    time = obj;
+  } else if(obj) {
+    time = new Date(obj);
+  } else {
+    time = new Date();
+  }
+
+  let result = {};
+
+  let [,month,day,year] = time.toDateString().split(' ');
+
+  month = C.MONTH[month];
+  result.month = parseInt(month);
+  result.year = parseInt(year);
+  result.day = parseInt(day);
+  result.time = time.getTime();
+  result.hours = time.getHours();
+  result.minutes = time.getMinutes();
+  result.seconds = time.getSeconds();
+
+  day = day < 10 ? '0' + day : day;
+  month = month < 10 ? '0' + month : month;
+
+  result.fullDate = year + '-' + month + '-' + day;
+
+  return result;
 }
 
 function empty(obj) {
